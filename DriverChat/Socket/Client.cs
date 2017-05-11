e09192ebb7113aeb648b17aa55d3b4fd74397d2c
823046544 <823046544@qq.com>
2017-05-11 14:53:50 +0800

    基本完成调试

--- DriverChat/Socket/Client.cs
+++ DriverChat/Socket/Client.cs
@@ -70,7 +70,6 @@ namespace DriverChat.Socket {
 
         List<int> Room_list = new List<int>();
         List<int> Driver_list = new List<int>();
-        bool plug;
         public Client(string _port, string HostIp) {
             serverPort = _port;
             working = true;
@@ -86,20 +85,142 @@ namespace DriverChat.Socket {
 
         public async void Listener() {
             try {
-
-                //GotMessage(0, "connect succeed");    
+                byte[] last = new byte[maxn];
+                int s, t, res_len = 0;
+                int count, st = 0, c_index = 0, index = 0;
+                byte[] c = new byte[maxn * 10];
+                bool isFile = false;
+                byte[] first = new byte[1];
+                byte[] Imgbytes = new byte[1];
+                string str_msg="";
+                JObject list= (JObject)JsonConvert.DeserializeObject("");
                 streamIn = clientsocket.InputStream.AsStreamForRead();
                 reader = new StreamReader(streamIn);
                 while (working) {
-                    int count, st = 0;
-                    byte[] c = new byte[maxn * maxn * 10 + 300];
-                    byte[] first = new byte[maxn];
                     bool flag = false;
                     count = await streamIn.ReadAsync(first, 0, first.Length);
-                    for (int i = 0; i < maxn; i++)
-                        c[st * maxn + i] = first[i];
+                    s = -1;
+
+                    while (s < count-1) {
+                        s++;
+                        if (isFile) {
+                            res_len = res_len-1;
+                            if (res_len != 0) {
+                                Imgbytes[index++] = first[s];
+                                continue;
+                            }
+                        } else {
+                            c[c_index++] = first[s];
+                            str_msg = System.Text.Encoding.UTF8.GetString(c);
+                            if (str_msg.IndexOf('\n') == -1) continue;
+                        }
+
+                        ///handler event
+                        if (!isFile)
+                            list = (JObject)JsonConvert.DeserializeObject(str_msg);
+                        if (list["type"].ToString() == "sys") {                                         ///handle system response
+                            if (list["detail"].ToString() == "sign in") {                                   ///handle signin
+                                status = list["status"].ToString() == "True" ? true : false;
+                                msg = list["msg"].ToString();
+                                if (status) {
+                                    did = Convert.ToInt32(list["driver"]["did"].ToString());
+                                    name = list["driver"]["name"].ToString();
+                                    badge = list["driver"]["badge"].ToString();
+                                    created_at = list["driver"]["created_at"].ToString();
+                                    this.GotSigninSucceed(msg);
+                                } else {                                                                    ///handle singin error
+                                    GotSigninError(msg);
+                                }
+                            } else if (list["detail"].ToString() == "sign up") {                            ///handle signup
+                                status = list["status"].ToString() == "True" ? true : false;
+                                msg = list["msg"].ToString();
+                                if (!status) {                                                              ///handle signup error
+                                    GotSignupError(msg);
+                                } else
+                                    GotSignupSucceed(msg);
+                            } else if (list["detail"].ToString() == "room list") {                          ///handle roomlist (need test)
+
+                                List<int> temp_list = new List<int>();
+
+                                foreach (var item in list["rooms"]) {
+                                    temp_list.Add(Convert.ToInt32(item["rid"].ToString()));
+                                    GotRoom(Convert.ToInt32(item["rid"].ToString()), item["name"].ToString(), item["direction"].ToString(),
+                                            Convert.ToInt32(item["activeness"].ToString()), item["created_at"].ToString());
+                                    //GotMessage(0, item.ToString());
+                                }
+                                foreach (var i in Room_list) {
+                                    flag = false;
+                                    foreach (var j in temp_list)
+                                        if (i == j)
+                                            flag = true;
+                                    if (!flag)
+                                        LostRoom(i, "", "", 0, "");
+                                }
+                                Room_list = temp_list;
+                                DriverChat.ViewModels.RoomViewModel.CreateView().Add_ALL_Pics();
+                            } else if (list["detail"].ToString() == "driver list") {                        ///handler driver list (need test)
+                                List<int> temp_list = new List<int>();
+                                if (Convert.ToInt32(list["rid"].ToString()) == cur_rid) {
+                                    foreach (var item in list["drivers"]) {
+                                        temp_list.Add(Convert.ToInt32(item["did"].ToString()));
+                                        GotDriver(Convert.ToInt32(list["rid"].ToString()), Convert.ToInt32(item["did"].ToString()),
+                                                  item["name"].ToString(), item["badge"].ToString());
+                                    }
+                                    foreach (int i in Driver_list) {
+                                        flag = false;
+                                        foreach (int j in temp_list)
+                                            if (i == j)
+                                                flag = true;
+                                        if (!flag)
+                                            LostDriver(cur_rid, i, "", "");
+                                    }
+                                    Driver_list = temp_list;
+
+                                    GotDriverList();
+                                }
+                            } else if (list["detail"].ToString() == "enter room") {                         ///handle enter room
+                                cur_rid = Convert.ToInt32(list["rid"].ToString());
+                            }
+                        } else if (list["type"].ToString() == "chat") {                                     ///handle chat
+                            int chat_from = Convert.ToInt32(list["from"].ToString());
+                            int chat_to = Convert.ToInt32(list["to"].ToString());
+                            string chat_msg = list["msg"].ToString();
+                            if (chat_to == cur_rid)
+                                GotMessage(chat_from, chat_msg);
+                        } else if (list["type"].ToString() == "file") {                                     ///handle file(img)
+                            if (!isFile) {
+                                string format = list["format"].ToString();
+                                res_len = Convert.ToInt32(list["length"].ToString());
+                                Imgbytes = new byte[res_len];
+                                index = 0;
+                                isFile = true;
+                                res_len += 1;
+                            } else {
+                                var image = new BitmapImage();
+                                using (InMemoryRandomAccessStream imgstream = new InMemoryRandomAccessStream()) {
+                                    await imgstream.WriteAsync(Imgbytes.AsBuffer());
+                                    imgstream.Seek(0);
+                                    await image.SetSourceAsync(imgstream);
+                                }
+                                if (list["detail"].ToString() == "driver avatar") {
+                                    GotDriverAvatar(Convert.ToInt32(list["driver"]["did"].ToString()), image);
+                                } else if (list["detail"].ToString() == "room avatar") {
+                                    GotRoomAvatar(Convert.ToInt32(list["room"]["rid"].ToString()), image);
+                                } else {
+                                    GotChatImage(Convert.ToInt32(list["from"].ToString()), image);
+                                }
+                                isFile = false;
+                            }
+                        }
+
+                        c_index = 0;
+                        Array.Clear(c, 0, c.Length);
+                    }
+
+                    /*
+                    for (int i = 0; i < count; i++) c[st * maxn + i] = first[i];
                     st++;
-                    string str_msg = System.Text.Encoding.UTF8.GetString(first);
+                    str_msg = System.Text.Encoding.UTF8.GetString(first);
                     if (str_msg[0] != '{' || str_msg == null)
                         continue;
                     string response = "";
@@ -122,7 +243,7 @@ namespace DriverChat.Socket {
                     }
                     if (response == null)
                         continue;
-                    JObject list = (JObject)JsonConvert.DeserializeObject(response);
+                    list = (JObject)JsonConvert.DeserializeObject(response);
                     if (list["type"].ToString() == "sys") {                                         ///handle system response
                         if (list["detail"].ToString() == "sign in") {                                   ///handle signin
                             status = list["status"].ToString() == "True" ? true : false;
@@ -196,18 +317,18 @@ namespace DriverChat.Socket {
                         string format = list["format"].ToString();
                         int length = Convert.ToInt32(list["length"].ToString());
                         byte[] json_bytes = System.Text.Encoding.UTF8.GetBytes(response);
-                        int index = (json_bytes.Length - 1) % maxn;
+                        index = (json_bytes.Length - 1) % maxn;
                         int res_first = count - 1 - index;
-                        int res_len = length - res_first;
-                        byte[] Imgbytes = new byte[length];
+                        res_len = length - res_first;
+                        Imgbytes = new byte[length];
                         for (int i = 0; i < res_first; i++)
                             Imgbytes[i] = c[i + index + 1];
                         index = res_first;
+                        res_len += 1;
                         while (res_len > 0) {
                             count = await streamIn.ReadAsync(first, 0, min(res_len, first.Length));
+                            for (int i = 0; i < min(count, res_len-1); i++) Imgbytes[index++] = first[i];
                             res_len -= count;
-                            for (int i = 0; i < count; i++)
-                                Imgbytes[index++] = first[i];
                         }
                         var image = new BitmapImage();
                         using (InMemoryRandomAccessStream imgstream = new InMemoryRandomAccessStream()) {
@@ -223,6 +344,7 @@ namespace DriverChat.Socket {
                             GotChatImage(Convert.ToInt32(list["from"].ToString()), image);
                         }
                     }
+                    */
 
                 }
             } catch (Exception ee) {
@@ -287,25 +409,15 @@ namespace DriverChat.Socket {
         }
 
         public void Create_Chat_Image_json(int len, byte[] Imgbytes, int rid) {
-            //msg = "{\"type\":\"file\",\"format\":\"image\",\"length\":" + len.ToString() + ",\"to\":" + room_num.ToString() + "}" + "\r\n";
-            /*
-            {
-                "type": "file",
-                "updown": "up",
-                "format": "image",
-                "length": len.ToString(),
-                "driver" : {
-                    "did": did.ToString()
-                },
-                "room": {
-                    "rid": rid.ToString()
-                },
-                "from": did.ToString(),
-                "to": cur_rid.ToString
-            }
-            */
             msg = "{\"type\":\"file\",\"updown\":\"up\",\"format\":\"image\",\"detail\": \"chat\",\"length\":" + len.ToString() + ",\"driver\":{\"did\":\"" + did.ToString() + "\"},\"room\":{\"rid\":\"" + rid.ToString() + "\"},\"from\":\"" + did.ToString() + "\",\"to\":\"" + cur_rid.ToString() + "\"}";
-            msg += "\r\n";
+            msg += "\n";
+            this.Send_Msg_Img(len, Imgbytes);
+        }
+
+
+        public void Update_User_Avatar(int len, byte[] Imgbytes) {
+            msg = "{\"type\":\"file\",\"updown\":\"up\",\"format\":\"image\",\"detail\": \"driver avatar\",\"length\":" + len.ToString() + ",\"driver\":{\"did\":\"" + did.ToString() + "\"}}";
+            msg += "\n";
             this.Send_Msg_Img(len, Imgbytes);
         }
 
@@ -326,12 +438,14 @@ namespace DriverChat.Socket {
         async public void Send_Msg_Img(int len, byte[] Imgbytes) {
             streamOut = clientsocket.OutputStream.AsStreamForWrite();
             byte[] json_bytes = System.Text.Encoding.UTF8.GetBytes(msg);
-            byte[] combine = new byte[json_bytes.Length + len];
+            byte[] enter_n = System.Text.Encoding.UTF8.GetBytes("\n");
+            byte[] combine = new byte[json_bytes.Length + len + 1];
             int json_count = json_bytes.Length;
             for (int i = 0; i < json_count; i++)
                 combine[i] = json_bytes[i];
             for (int i = 0; i < len; i++)
                 combine[i + json_count] = Imgbytes[i];
+            combine[combine.Length-1] = enter_n[0];
 
             await streamOut.WriteAsync(combine, 0, combine.Length);
             await streamOut.FlushAsync();
