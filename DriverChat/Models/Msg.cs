bc97bb5d24701e334d67cc95c114128225ef261a
Sevennn <418057982@qq.com>
2017-05-14 22:09:30 +0800

    A little diff

--- DriverChat/Models/Msg.cs
+++ DriverChat/Models/Msg.cs
@@ -6,13 +6,13 @@ using System.Threading.Tasks;
 using Windows.UI.Xaml.Media;
 
 namespace DriverChat.Models {
-    class Msg {
-        public string Comment { get; set; }
-        public ImageSource HeadPic { get; set; }
-        public bool IsSelf { get; set; }
-        public bool IsPic { get; set; }
-        public ImageSource MsgPic { get; set; }
-        public string username { get; set; }
-    }
+  class Msg {
+    public string Comment { get; set; }
+    public ImageSource HeadPic { get; set; }
+    public bool IsSelf { get; set; }
+    public bool IsPic { get; set; }
+    public ImageSource MsgPic { get; set; }
+    public string username { get; set; }
+  }
 
 }
--- DriverChat/Signup.xaml.cs
+++ DriverChat/Signup.xaml.cs
@@ -16,48 +16,43 @@ using Windows.UI.Xaml.Navigation;
 
 // “空白页”项模板在 http://go.microsoft.com/fwlink/?LinkId=234238 上有介绍
 
-namespace DriverChat
-{
-    /// <summary>
-    /// 可用于自身或导航至 Frame 内部的空白页。
-    /// </summary>
-    public sealed partial class Signup : Page
-    {
-        public Signup()
-        {
-            this.InitializeComponent();
-        }
-
-        private void Confirm(object sender, RoutedEventArgs e)
-        {
-            DriverChat.Socket.Client.GetClient().Create_Signup_json(UserName.Text, Password.Text, NickName.Text, DateTimeOffset.Now.ToString());
-        }
-
-        protected override void OnNavigatedTo(NavigationEventArgs e) {
-            DriverChat.Socket.Client.GetClient().GotSignupError += Error;
-            DriverChat.Socket.Client.GetClient().GotSignupSucceed += Succeed;
-        }
-
-        protected override void OnNavigatedFrom(NavigationEventArgs e) {
-            DriverChat.Socket.Client.GetClient().GotSignupError -= Error;
-            DriverChat.Socket.Client.GetClient().GotSignupSucceed -= Succeed;
-        }
-
-        private async void Error(string msg) {
-            MessageDialog t = new MessageDialog(msg);
-            await t.ShowAsync();
-        }
-
-        private async void Succeed (string msg) {
-            MessageDialog t = new MessageDialog("注册成功");
-            await t.ShowAsync();
-            Frame.Navigate(typeof(Login));
-        }
-
-        private void Quit(object sender, RoutedEventArgs e)
-        {
-            Frame.Navigate(typeof(Login));
-        }
-
+namespace DriverChat {
+  /// <summary>
+  /// 可用于自身或导航至 Frame 内部的空白页。
+  /// </summary>
+  public sealed partial class Signup : Page {
+    public Signup() {
+      this.InitializeComponent();
     }
+
+    private void Confirm(object sender, RoutedEventArgs e) {
+      DriverChat.Socket.Client.GetClient().Create_Signup_json(UserName.Text, Password.Text, NickName.Text, DateTimeOffset.Now.ToString());
+    }
+
+    protected override void OnNavigatedTo(NavigationEventArgs e) {
+      DriverChat.Socket.Client.GetClient().GotSignupError += Error;
+      DriverChat.Socket.Client.GetClient().GotSignupSucceed += Succeed;
+    }
+
+    protected override void OnNavigatedFrom(NavigationEventArgs e) {
+      DriverChat.Socket.Client.GetClient().GotSignupError -= Error;
+      DriverChat.Socket.Client.GetClient().GotSignupSucceed -= Succeed;
+    }
+
+    private async void Error(string msg) {
+      MessageDialog t = new MessageDialog(msg);
+      await t.ShowAsync();
+    }
+
+    private async void Succeed(string msg) {
+      MessageDialog t = new MessageDialog("注册成功");
+      await t.ShowAsync();
+      Frame.Navigate(typeof(Login));
+    }
+
+    private void Quit(object sender, RoutedEventArgs e) {
+      Frame.Navigate(typeof(Login));
+    }
+
+  }
 }
